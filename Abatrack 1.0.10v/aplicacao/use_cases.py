9f03c693036c7d1230f6f26b3d3a191090151511
katarynecunha28 <79368588+katarynecunha28@users.noreply.github.com>
2026-06-23 13:57:10 -0300

    adiciona informações extra no txt

--- Abatrack 1.0.10v/aplicacao/use_cases.py
+++ Abatrack 1.0.10v/aplicacao/use_cases.py
@@ -48,7 +48,7 @@ def salvarDadosTXT(repositorio: RepositorioTelemetria):
         file.write("-------------------------------\n")
 
         for pacote in repositorio.pacotesDados:
-            file.write(f"{pacote[0]}°C  {pacote[1]}s  {pacote[2]}Pa  {pacote[3]}m  latitude:{pacote[4]}  longitude:{pacote[5]}  n° de sats:{pacote[6]}  giro x:{pacote[7]}  giro y:{pacote[8]}  giro z:{pacote[9]}  n° de pacotes:{pacote[10]}  RSSI:{pacote[11]} dBm  tamanho do pacote:{pacote[12]}\n")
+            file.write(f"{pacote[0]}s  {pacote[1]}°C   {pacote[2]}%  {pacote[4]}Pa  {pacote[3]}m latitude:{pacote[5]} longitude:{pacote[6]}  n° de sats:{pacote[7]}  giro x:{pacote[8]}  giro y:{pacote[9]}  giro z:{pacote[10]} temperatura bateria 1:{pacote[11]}°C temperatura bateria 2:{pacote[12]}°C tensão:{pacote[13]}V corrente:{pacote[14]}A n° de pacotes:{pacote[15]}  RSSI:{pacote[16]} dBm  tamanho do pacote:{pacote[17]}\n")
     print("Histórico salvo no TXT")
 
 
