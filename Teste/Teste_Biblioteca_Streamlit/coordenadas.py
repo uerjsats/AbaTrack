a27884fb6e39ac786eb1e58f34b8fffc81daa88a
katarynecunha28 <almeida.kataryne@graduacao.uerj.br>
2026-08-02 16:22:28 -0300

    refactor: padroniza estrutura na raiz, responsividade e adiciona .gitignore

--- Teste/Teste_Biblioteca_Streamlit/coordenadas.py
+++
@@ -1,8 +0,0 @@
-class Coordenadas:
-    @staticmethod
-    def get_fixed_coordinates():
-        # Coordenadas fixas para teste
-        return {"latitude": -23.55052, "longitude": -46.633308}  # São Paulo, Brasil
-
-
-
